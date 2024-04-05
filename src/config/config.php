5c98e06eb506373724938adac4d12cc3b3832730
ogabrielmacedo <114434761+ihawkeye@users.noreply.github.com>
2024-04-04 23:37:39 -0300

    Add files via upload

---
+++ src/config/config.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+class Database {
+    private $host;
+    private $usuario = 'SYSDBA';
+    private $senha = 'masterkey';
+    private $conn;
+    
+    public function getConnection() {
+        // Cria a conexão
+        try {
+            $this->conn = new PDO(
+                "firebird:dbname=localhost:C:\CPlus\CPLUS.FDB;charset=UTF8",
+                $this->usuario,
+                $this->senha
+            );
+        } catch(PDOException $e) {
+            echo "Falha na conexão." . $e->getMessage();
+        }
+        
+        return $this->conn;
+    }
+
+		public function setHost($caminho){
+			$this->host = $caminho;
+		}
+
+
+	}	
+
+?>
