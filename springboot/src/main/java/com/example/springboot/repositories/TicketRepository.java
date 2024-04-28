2eeb96e67d7cea2cd263b389617dcc5cf6982f0b
MathLPblue <147007125+MathLPblue@users.noreply.github.com>
2024-04-28 12:50:38 -0300

    Delete springboot directory

--- springboot/src/main/java/com/example/springboot/repositories/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.repositories;
-
-import com.example.springboot.models.TicketModel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface TicketRepository extends JpaRepository<TicketModel, Integer> {
-}
